c61cdbb92abcdd1e06c49c16f354bef6238a3905
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-02-08 00:26:34 +0700

    rename welcome to index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    @livewireStyles
+    <title>Belajar Livewire</title>
+</head>
+
+<body>
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            @livewire('show-index')
+        </div>
+    </div>
+
+
+    @livewireScripts
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <style>
+        body {
+            background: rgb(34, 120, 251);
+            background: radial-gradient(circle, rgba(34, 120, 251, 0.7455357142857143) 0%, rgba(115, 196, 194, 0.7679446778711485) 100%);
+        }
+    </style>
+</body>
+
+</html>
